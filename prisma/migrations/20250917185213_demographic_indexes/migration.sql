7782f0462c5f234a586bad9994a8e4db6e65289c
Tomer Almog <tomer@goodparty.org>
2025-09-29 20:04:10 -0400

    another fix attempt

--- prisma/migrations/20250917185213_demographic_indexes/migration.sql
+++ prisma/migrations/20250917185213_demographic_indexes/migration.sql
@@ -1,32 +1,32 @@
 -- CreateIndex
-CREATE INDEX "Voter_Business_Owner_idx" ON "Voter"("Business_Owner");
+CREATE INDEX CONCURRENTLY "Voter_Business_Owner_idx" ON "Voter"("Business_Owner");
 
 -- CreateIndex
-CREATE INDEX "Voter_Education_Of_Person_idx" ON "Voter"("Education_Of_Person");
+CREATE INDEX CONCURRENTLY "Voter_Education_Of_Person_idx" ON "Voter"("Education_Of_Person");
 
 -- CreateIndex
-CREATE INDEX "Voter_Estimated_Income_Amount_idx" ON "Voter"("Estimated_Income_Amount");
+CREATE INDEX CONCURRENTLY "Voter_Estimated_Income_Amount_idx" ON "Voter"("Estimated_Income_Amount");
 
 -- CreateIndex
-CREATE INDEX "Voter_Homeowner_Probability_Model_idx" ON "Voter"("Homeowner_Probability_Model");
+CREATE INDEX CONCURRENTLY "Voter_Homeowner_Probability_Model_idx" ON "Voter"("Homeowner_Probability_Model");
 
 -- CreateIndex
-CREATE INDEX "Voter_Language_Code_idx" ON "Voter"("Language_Code");
+CREATE INDEX CONCURRENTLY "Voter_Language_Code_idx" ON "Voter"("Language_Code");
 
 -- CreateIndex
-CREATE INDEX "Voter_Marital_Status_idx" ON "Voter"("Marital_Status");
+CREATE INDEX CONCURRENTLY "Voter_Marital_Status_idx" ON "Voter"("Marital_Status");
 
 -- CreateIndex
-CREATE INDEX "Voter_Presence_Of_Children_idx" ON "Voter"("Presence_Of_Children");
+CREATE INDEX CONCURRENTLY "Voter_Presence_Of_Children_idx" ON "Voter"("Presence_Of_Children");
 
 -- CreateIndex
-CREATE INDEX "Voter_Registered_Voter_idx" ON "Voter"("Registered_Voter");
+CREATE INDEX CONCURRENTLY "Voter_Registered_Voter_idx" ON "Voter"("Registered_Voter");
 
 -- CreateIndex
-CREATE INDEX "Voter_Veteran_Status_idx" ON "Voter"("Veteran_Status");
+CREATE INDEX CONCURRENTLY "Voter_Veteran_Status_idx" ON "Voter"("Veteran_Status");
 
 -- CreateIndex
-CREATE INDEX "Voter_Voter_Status_idx" ON "Voter"("Voter_Status");
+CREATE INDEX CONCURRENTLY "Voter_Voter_Status_idx" ON "Voter"("Voter_Status");
 
 -- CreateIndex
-CREATE INDEX "Voter_Voter_Status_UpdatedAt_idx" ON "Voter"("Voter_Status_UpdatedAt");
+CREATE INDEX CONCURRENTLY "Voter_Voter_Status_UpdatedAt_idx" ON "Voter"("Voter_Status_UpdatedAt");
