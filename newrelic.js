753b08992f9a8e0f35482e41e3bc85b8fcdbaf2c
oakinh <nikaohill@gmail.com>
2025-11-21 11:55:10 -0600

    Add newrelic

---
+++ newrelic.js
@@ -0,0 +1,45 @@
+'use strict'
+if (!process.env.NEW_RELIC_APP_NAME || !process.env.NEW_RELIC_LICENSE_KEY) {
+  console.warn(
+    'New Relic disabled: Missing NEW_RELIC_APP_NAME or NEW_RELIC_LICENSE_KEY',
+  )
+  module.exports = {}
+} else {
+  exports.config = {
+    app_name: [process.env.NEW_RELIC_APP_NAME || 'people-api'],
+    license_key: process.env.NEW_RELIC_LICENSE_KEY,
+    logging: {
+      level: process.env.NEW_RELIC_LOG_LEVEL || 'warn',
+      filepath: 'stdout',
+    },
+    distributed_tracing: { enabled: true },
+    application_logging: {
+      enabled: true,
+      forwarding: { enabled: false },
+      metrics: { enabled: true },
+      local_decorating: { enabled: true },
+    },
+    error_collector: {
+      enabled: true,
+      ignore_status_codes: [401],
+    },
+    allow_all_headers: true,
+    attributes: {
+      exclude: [
+        'request.headers.cookie',
+        'request.headers.authorization',
+        'request.headers.proxyAuthorization',
+        'request.headers.setCookie*',
+        'request.headers.x*',
+        'response.headers.cookie',
+        'response.headers.authorization',
+        'response.headers.proxyAuthorization',
+        'response.headers.setCookie*',
+        'response.headers.x*',
+      ],
+    },
+    rules: {
+      ignore: ['^/v1/health$'],
+    },
+  }
+}
